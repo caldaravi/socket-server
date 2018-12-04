c684a191b23b66824bff9f75c93b1fcc3dfd8a24
Carlos <carlosaldaravi@gmail.com>
2018-12-04 22:05:35 +0100

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,21 @@
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+
+
+
+
+const server = new Server();
+
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true }));
+server.app.use( bodyParser.json() );
+
+server.app.use('/', router);
+
+
+server.start( () => {
+    console.log(`Servidor inicializado en el puerto ${ server.port }`);
+})
+
+
